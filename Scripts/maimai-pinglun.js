fa1d08167b3f1aa154b73fd7d781a3c2561cdc85
huzy96 <huzhaoyun96@outlook.com>
2024-04-07 09:40:07 +0800

    更新脉脉评论广告过滤

--- Scripts/maimai-pinglun.js
+++ Scripts/maimai-pinglun.js
@@ -11,6 +11,17 @@ if (obj.hasOwnProperty('lst')) {
     }
 }
 
+if (obj.hasOwnProperty('comments')) {
+    if (obj.hasOwnProperty('lst')) {
+        for (let i = 0; i < obj['lst'].length; i++) {
+            if (obj['lst'][i].hasOwnProperty('newAdStyle')) {
+                obj['lst'].splice(i, 1);
+                i--;
+            }
+        }
+    }
+}
+
 body = JSON.stringify(obj);
 console.log(body);
 $done(body);
